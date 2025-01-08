1454c3cc80aa5c199d64521636dd69be092c67a5
Lakota Camp <lakotacamp3@gmail.com>
2025-01-08 09:28:08 -0800

    chore: shadcn setup complete, react-router setup complete, chart demos complete. Todo: - Dashboard template - Page router layout - Header and Nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,57 @@
-import './App.css'
+import "./App.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import { DemoChart } from "./charts/DemoBarChart";
+import { DemoAreaGraph } from "./charts/DemoAreaGraph";
+import { PieDonut } from "./charts/DemoPieDonut";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <>
-      <h1>LPL Hackathon 2025</h1>
-    </>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <h1 className="text-center text-5xl font-bold p-8">LPL Hackathon 2025</h1>
+      <div className="p-4 text-center">
+        <ModeToggle />
+      </div>
+      <Router>
+        <div className="p-4 text-center">
+          <nav>
+            <ul className="flex justify-center gap-4">
+              <li className="p-2">
+                <Button asChild>
+                  <Link to="/">Home</Link>
+                </Button>
+              </li>
+              <li className="p-2">
+                <Button asChild>
+                  <Link to="/chart-one">Chart One</Link>
+                </Button>
+              </li>
+              <li className="p-2">
+                <Button asChild>
+                  <Link to="/chart-two">Chart Two</Link>
+                </Button>
+              </li>
+              <li className="p-2">
+                <Button asChild>
+                  <Link to="/chart-three">Chart Three</Link>
+                </Button>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/chart-one" element={<DemoChart />} />
+            <Route path="/chart-two" element={<DemoAreaGraph />} />
+            <Route path="/chart-three" element={<PieDonut />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
